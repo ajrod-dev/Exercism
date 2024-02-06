014dd9f8507ff6c102290c60daf5231ab62e3988
ajrod-dev <alexander.joshua.rodriguez@gmail.com>
2024-02-06 14:42:02 -0700

    boutique inventory problem

--- boutique_inventory.rb
+++ boutique_inventory.rb
@@ -1,50 +1,33 @@
+require 'ostruct'
 class BoutiqueInventory
+  attr_reader :items
+
   def initialize(items)
-    @items = items
+    @items = items.map {|item| OpenStruct.new(item)}
   end
 
   def item_names
-    @items.map { |item|
-      item[:name]
-    }.sort
-  end
-
-  def cheap
-    @items.select { |item, name|
-      item[:price] < 30
-    }
-  end
-
-  def out_of_stock
-    @items.select { |item, name|
-      item[:quantity_by_size].empty? 
-    }
-  end
-
-  def stock_for_item(name)
-    item = @items.find { |item| item[:name] == name }
-    item ? item[:quantity_by_size] : {}
+    items.map(&:name).sort
 
   end
 
   def total_stock
-    total = 0
-    @items.sum { |item|
-      item[:quantity_by_size].sum {|_,total| total }
-    }
-  end
+    items.map(&:quantity_by_size).map{|sizes| sizes.values.sum}.sum
+    # => (Another example) items.map(&:quantity_by_size).map(&:values).sum(&:sum)
+    # => (Another example) items.map(&:quantity_by_size).map(&:values).flatten.sum
 
-  private
-  attr_reader :items
+  end
 end
 
 
-puts BoutiqueInventory.new([
+inventory = BoutiqueInventory.new([
   {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
   {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
   {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
   {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
-]).total_stock
+])
+
+puts inventory.total_stock.inspect
 
 
 
---
+++ boutique_inventory_original.rb
@@ -0,0 +1,47 @@
+class BoutiqueInventory
+  def initialize(items)
+    @items = items
+  end
+
+  def item_names
+    @items.map { |item|
+      item[:name]
+    }.sort
+  end
+
+  def cheap
+    @items.select { |item, name|
+      item[:price] < 30
+    }
+  end
+
+  def out_of_stock
+    @items.select { |item, name|
+      item[:quantity_by_size].empty? 
+    }
+  end
+
+  def stock_for_item(name)
+    item = @items.find { |item| item[:name] == name }
+    item ? item[:quantity_by_size] : {}
+
+  end
+
+  def total_stock
+    total = 0
+    @items.sum { |item|
+      item[:quantity_by_size].sum {|_,total| total }
+    }
+  end
+
+  private
+  attr_reader :items
+end
+
+
+puts BoutiqueInventory.new([
+  {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+  {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
+  {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+  {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+]).total_stock
